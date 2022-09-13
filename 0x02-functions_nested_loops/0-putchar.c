7b0097b8ded473e1b56c5233bd76507dc8cbe19c
OGONYI <osimogonyi@gmail.com>
2022-09-13 12:39:29 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "alx.h"
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+* Return: On success 0.
+* On error, -0 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(0, &c, 0));
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX C-functions and nestedd loops
