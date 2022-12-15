5df67dffaaf78c2838d6ea64806282027dd3e59b
OGONYI <osimogonyifx@gmail.com>
2022-12-15 07:03:25 -0800

    Task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/OGONYI/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=../tmp/libhack.so
+wget -P ./ https://github.com/OGONYI/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=./tmp/libhack.so
